10f59e11b024de6807aa495482b31bfe23323d25
chrisli8 <8chrisgli@gmail.com>
2017-01-16 17:03:51 -0800

    adding files

---
+++ app/src/main/java/activityspy/chrisli8/washington/edu/activityspy/MainActivity.java
@@ -0,0 +1,46 @@
+package activityspy.chrisli8.washington.edu.activityspy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart event fired");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume event fired");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause event fired");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop event fired");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "We’re going down, Captain!");
+    }
+}
